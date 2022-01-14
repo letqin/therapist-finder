625df4d84886568bed50713d3bd57caecbda7dd0
Kronk <kylec0217@gmail.com>
2022-01-14 02:34:38 -0500

    added queries to find a therapist by first and last name, specialty and location, and an update therapist mutation that doesnt work quite right

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -19,6 +19,19 @@ const resolvers = {
     therapist: async (parent, {therapistId}) => {
       return Therapist.findOne({_id: therapistId});
     },
+    therapistFirst: async (parent, {firstName}) => {
+      return Therapist.findOne({first: firstName})
+    },
+    therapistLast: async (parent, {lastName}) => {
+      return Therapist.findOne({last: lastName})
+    },
+    therapistSpecialty: async (parent, {speciality}) => {
+      return Therapist.findOne({speciality: speciality})
+    },
+    therapistLoc: async (parent, {location}) => {
+      return Therapist.findOne({location: location})
+    },
+
     // By adding context to our query, we can retrieve the logged in user without specifically searching for them
     me: async (parent, args, context) => {
       if (context.user) {
@@ -55,6 +68,24 @@ const resolvers = {
     addTherapist: async (parent, {first, middle, last, speciality, time, location, takeInsurance, privatePay})=>{
       const therapist = await Therapist.create({first, middle, last, speciality, time, location, takeInsurance, privatePay})
 
+      return therapist
+    },
+
+    updateTherapist: async (parent, {id, first, middle, last, specialty, time, location, takeInsurance, privatePay}) => {
+      //calls a therapist that we can update
+      const therapist = await Therapist.find({'_id' : id})
+      //gives each part of the schema the value given
+      therapist.first = first
+      therapist.middle = middle
+      therapist.last = last
+      therapist.specialty = specialty
+      therapist.time = time
+      therapist.location = location
+      therapist.takeInsurance = takeInsurance
+      therapist.privatePay = privatePay
+      //saves all the values we just set
+      await therapist.save
+      //returns our updated therapist object
       return therapist
     },
 
