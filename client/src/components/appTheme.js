3fccc1c3f21062737d56c98eaad0fbf5ae368c6f
Wafa Mohessin <wafamohessin@gmail.com>
2022-01-14 11:33:23 -0500

    umi css pushed

---
+++ client/src/components/appTheme.js
@@ -0,0 +1,20 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#71c9e9',
+      main: '#636aa4',
+      dark: '#314b98',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#f99b35',
+      main: '#e06c91',
+      dark: '#58595b',
+      contrastText: '#fff',
+    },
+  },
+});
+
+
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,25 +1,93 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import TherapistsCard from '../components/therapistsCard'
+import signinButton from '../components/signinButton'
 
 import { styled } from '@mui/material/styles';
+
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Button from '@mui/material/Button';
+import textfield from '@mui/material/textfield';
+import typography from '@mui/material/typography';
+
+import container from '@mui/material/container';
+import Paper from '@mui/material';
+
+
+import {makeStyles, ThemeProvider, createMuiTheme} from '@mui/material/makeStyles';
+
+import { indigo, gray } from '@mui/material/colors';
 
 import ProfileList from '../components/ProfileList';
 import { QUERY_THERAPISTS } from '../utils/queries';
+
+
+const useStyles = makeStyles ({
+  root:{
+    background: indigo,
+    border: 1,
+    borderRadius: 15,
+    color: 'white',
+    padding: '0 30px'
+  }
+})
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#71c9e9',
+      main: '#636aa4',
+      dark: '#314b98',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#f99b35',
+      main: '#e06c91',
+      dark: '#58595b',
+      contrastText: '#fff',
+    },
+  },
+});
+
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_THERAPISTS);
   const therapists = data?.therapists || [];
 
+
   return (
     
     <main>
-      
+      <ThemeProvider theme={theme}>
 
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
+      <typography variant="h2">
+        Search fot a therapist
+      </typography>
+
+      <buttonGroup variant="contained" color= '#58595b' size="medium">
+        <button>
+        SignIn
+        </button>
+        <button>
+        Sign Out
+        </button>
+      </buttonGroup>
+
+      <textfield 
+      variant= 'outlined'
+      color= '#e06c91'
+      //type="date"
+      />
+      
+    
+    <Box sx={{
+           boxShadow: 2,
+           bgcolor: '#636aa4', 
+           color: '#fff', p: 2 }}>
+
+      <Grid container spacing={3} justify="center">
         <Grid xs={8}>
         {loading ? (
             <div>Loading...</div>
@@ -27,9 +95,10 @@ const Home = () => {
             therapists.map(therapist => {
               
               return(
+               <div>
                 <TherapistsCard data={therapist} />
-
-             
+                <signinButton />
+                </div>
               )
             })
             // <ProfileList
@@ -41,15 +110,10 @@ const Home = () => {
         <Grid xs={4}>
           search form here
         </Grid>
-       
-    </Grid> 
+      </Grid>
     </Box>
-    
-      
-      
-
-          
-      
+             
+    </ThemeProvider>
     </main>
   );
 };
