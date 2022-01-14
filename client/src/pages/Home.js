0e6c2911b897a1e4bcd784ce4fda0f3ea5f8d2d2
Kronk <kylec0217@gmail.com>
2022-01-14 09:07:09 -0500

    moving the search card to its own file

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import TherapistsCard from '../components/therapistsCard'
+import SearchCard from '../components/searchCard'
 
 import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+
 import ProfileList from '../components/ProfileList';
 import { QUERY_THERAPISTS } from '../utils/queries';
-
-
-//creating a styled component for the search section
-const SearchCard = styled('section')({
-  display: 'flex',
-  flexDirection: 'column',
-  p: 1,
-  m: 1
-}); 
+import { CardContent } from '@mui/material';
 
 
 const Home = () => {
@@ -47,48 +40,7 @@ const Home = () => {
               )}
           </Grid>
           <Grid xs={4}>
-            <SearchCard>
-              <Box 
-                component="form"
-                sx={{
-                  '& > :not(style)': { m: 1, width: '25ch' },
-                }}
-                noValidate
-                autoComplete="off"
-              >
-                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-              </Box>
-              <Box 
-                component="form"
-                sx={{
-                  '& > :not(style)': { m: 1, width: '25ch' },
-                }}
-                noValidate
-                autoComplete="off"
-              >
-                <TextField id="filled-basic" label="Filled" variant="filled" />
-              </Box>
-              <Box 
-                component="form"
-                sx={{
-                  '& > :not(style)': { m: 1, width: '25ch' },
-                }}
-                noValidate
-                autoComplete="off"
-              >
-                <TextField id="standard-basic" label="Standard" variant="standard" />
-              </Box>
-              <Box 
-                component="form"
-                sx={{
-                  '& > :not(style)': { m: 1, width: '25ch' },
-                }}
-                noValidate
-                autoComplete="off"
-              >
-                <TextField id="standard-basic" label="Standard" variant="standard" />
-              </Box>
-            </SearchCard>
+            <SearchCard />
           </Grid>
         </Grid> 
       </Box>
