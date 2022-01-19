8d61a7ee7ffc5eb41302677057c1b4537a2b54e5
Kronk <kylec0217@gmail.com>
2022-01-19 11:26:20 -0500

    finishing client side queries and mutations and putting them in the api folder

--- client/src/pages/therapistPage.js
+++ client/src/pages/therapistPage.js
@@ -24,7 +24,7 @@ let TherapistPage = () => {
                         ) : (
                             
                                     <>
-                                        <Typography variant="h1">{therapists[0]}</Typography>
+                                        <Typography variant="h1">How </Typography>
                                         <Typography variant="h1"></Typography>
                                         <Typography variant="h1"></Typography>
                                         <Typography variant="h1"></Typography>
